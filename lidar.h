8841d46b14938dc3a383c19d2e464f17fb68760f
Evelyn Anyebe <anyebeevelyn@gmail.com>
2022-04-14 19:21:17 +0100

    Add documentation for doxygen to lidar

--- lidar.h
+++ lidar.h
@@ -13,26 +13,57 @@
 #include <chrono>
 #include "servo.h"
 
+
+/**
+* 
+* @section DESCRIPTION
+* This class represents the LiDAR sensor. 
+*/ 
+/**
+* @brief The LiDAR class is a low-level driver for obtaining distance values needed for the functioning of the blind navigation system.
+*
+* This class manages TFMini LiDAR sensor and couples the microcontroller such that the executable only needs.
+* to implement the data interface  callback method. It runs a thread and uses UART for serial communication.
+*   
+*/
 class Lidar{
     public:
-        // This is the number of distances to obtain from the lidar. 
-        //It represents a complete scan (the obstacle scan + 90 scan degree field of view)
+        /* This is the number of distances to obtain from the lidar. 
+        *  It represents a complete scan (the obstacle scan + 90 scan degree field of view)
+        */
         static const unsigned nDistance = 91;
         
         // This stores all the data received in a complete scan
         int LidarData[nDistance];
 
+        /**
+        * Constructor that sets the object to commence.
+        * 
+        * @param _doInit bool has default value: true
+        */ 
         Lidar(bool _doInit = true) {
 		    doInit = _doInit;
 	    }
 
-        // This is a function to initialize variables/settings needed by the library.
-        // It has a serial port argument which is "/dev/ttyS0" by default
+        /**
+        * This is a method to initialize variables and other settings needed by the library.
+        * @param serial_port const char* default  "/dev/ttyS0".
+        *
+        * @return void
+        */
         void start(const char *serial_port = "/dev/ttyS0");
         
-        // Stops the thread
+        /**
+        * This is a method join the Lidar thread, close serial port, and terminate gpio.
+        *
+        * @return void
+        */
         void stop();
         
+        /**
+        * Destructor to call stop() method and terminate gpio
+        *
+        */
         ~Lidar() {
             stop();
 		    gpioTerminate();
@@ -40,6 +71,7 @@ class Lidar{
 
         /**
         * Callback interface which needs to be implemented by the user.
+        *
         **/
         struct DataInterface {
             virtual void newScanAvail(int (&)[Lidar::nDistance]) = 0;
