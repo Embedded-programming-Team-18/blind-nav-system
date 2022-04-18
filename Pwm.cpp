a3ca0bec141765c5c31acbf7a984b877e39fa4d5
Evelyn <anyebeevelyn@gmail.com>
2022-04-18 08:01:25 +0100

    add pwm and servo test

--- Pwm.cpp
+++ Pwm.cpp
@@ -1,12 +1,16 @@
 #include "pwm.h"
+
 void Pwm::sendPwm(int gpioPin,int distance){
+    int pinPWM = 0;
+    
     if(distance>thresholdMaxDist){
         distance = thresholdMaxDist;
-    }else if(minValue<thresholdMinDist){
+    }else if(distance<thresholdMinDist){
         distance=0;
     }
+
     pinPWM = (thresholdMaxDist-distance)*255/thresholdMaxDist;
-    //std::cout<<"Min "<<minValue<<", Pwm "<<i<<": "<<pinPWM<<std::endl;
+
     gpioPWM(gpioPin,pinPWM);
 }
 
